5c05dbe95d5497ef438206802687e4737fb31b2d
Jason Park <jjpark987@gmail.com>
2022-10-25 10:38:50 -0700

    commit tree 961cf50f94bc3c4b184b49b54dde80db390b4a5c

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function superbowlWin(broncoRecord) {
+    for(const value of broncoRecord) {
+        if(value.result === `W`) {
+            return value.year;
+        }
+    }
+}
